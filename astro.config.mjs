4c1460441a644c9ae495d81958feb2bca30df644
Luis Fernando Lopez <105130698+luisfer194@users.noreply.github.com>
2023-07-13 01:13:26 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
   site: "https://luisfer194.github.io",
   base: "/books-luis",
 });
