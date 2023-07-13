4beaaf52c4dc04d5082694b2c008852ae83de7bc
luisfer194 <luissfer194@gmail.com>
2023-07-12 21:07:43 -0500

    Subiendo el Proyecto de Books-Luis

---
+++ src/data/books.js
@@ -0,0 +1,30 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: Manual de desarrollo agil de software',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		apinion: 'Un clasico entre los clasico del mundo de la programacion. Aunque en pleno 2023 yo tengo una opinion un poco polemica. No me parece un libro que recomendaria a todo el mundo, especialmente a aquellas personas que estan aprendiendo. Ademas, tiene un tono muy dogmatico y existe gente que se lo toma, peligrosamente, como la biblia del desarrollo. Pero si eres capaz de leerlo tomando perspectiva, hay cosas interesantes que todavia estan vigentes. Ojo que todos los ejemplos de codigo estan basados en Java y mucha programacion orientada a objetos',
+		author: 'Robert C. Martin'
+	},
+	{
+		id: 'the-clean-coder',
+		title: 'The clean Coder: A code of conduct for Professional Programmers',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+		apinion: '',
+		author: 'Robert C. Martin'
+	},
+	{
+		id: 'javascript-the-good-parts',
+		title: 'JavaScript: The Good Parts',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+		apinion: '',
+		author: 'Douglas Crockford'
+	},
+	{
+		id: 'eliquent-javascript',
+		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+		apinion: '',
+		author: 'Marijn Haverbeke'
+	}
+]
