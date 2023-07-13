d49888d0fc43abf23971bba7fb4366df57db37df
Luis Fernando Lopez <105130698+luisfer194@users.noreply.github.com>
2023-07-13 03:10:47 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import nodejs from '@astrojs/node';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: nodejs(),
-  output: "hybrid",
+  adapter: netlify(),
+  output: "server",
 });
