f2fe2a0b85093ce4ee31860791bd07b94ff84eb1
luisfer194 <luissfer194@gmail.com>
2023-07-13 01:54:25 -0500

    configurar Adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import nodejs from '@astrojs/node';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  adapter: netlify(),
+  output: "server"
 });
